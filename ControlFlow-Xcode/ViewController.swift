6ca9fff7e1f88548cb50d3a19c3d5a12df9a64f1
Ansel Adams <ansel@MacMini.local>
2016-01-31 12:25:43 -0700

    initial commit

--- ControlFlow-Xcode/ViewController.swift
+++ ControlFlow-Xcode/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let truckSpeed = 45
+        let lamboSpeed = 120
+        var mySpeed:Int
+        
+        //print(mySpeed)
+        mySpeed = 80
+        
+        if mySpeed < 70 {
+            print("Keep Cruising")
+        }
+        else {
+            print("Slow down police ahead")
+        }
+        
+        print("Hello, world!")
     }
 
     override func didReceiveMemoryWarning() {
